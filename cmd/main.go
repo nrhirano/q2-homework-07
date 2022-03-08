61d99b734a2ac3ecdb134bce1a93dcec6d608e9c
nishioad\WS5006hirano <ryuuji.hirano@nishio-rent.co.jp>
2022-03-08 09:20:51 +0900

    feat : NG_BACK_END-230

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
+	a := []int{0, 1, 2}
 	var out []*int
 	for i := 0; i < 3; i++ {
 		fmt.Println(i)
-		out = append(out, &i)
+		out = append(out, &a[i])
+		log.Println(out)
 	}
 	for _, o := range out {
 		fmt.Println(*o)
+		log.Println(*o)
 	}
 }
